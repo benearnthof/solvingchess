f66b4557f4dd93a11220d136f82d6220a512f5e5
unknown <benearnthof@hotmail.de>
2020-11-10 18:06:16 +0100

    add literature

---
+++ literature.py
@@ -0,0 +1,7 @@
+# literature overview
+# mastering the game of go 
+# https://www.nature.com/articles/nature24270.epdf?author_access_token=VJXbVjaSHxFoctQQ4p2k4tRgN0jAjWel9jnR3ZoTv0PVW4gB86EEpGqTRDtpIz-2rmo8-KG06gqVobU5NSCFeHILHcVFUeMsbvwS-lxjqQGg98faovwjxeTUgZAUMnRQ
+# mastering chess and shogi by self play 
+# https://arxiv.org/pdf/1712.01815.pdf
+# neural chess implementation in python
+# https://github.com/maciejczyzewski/neural-chessboard
