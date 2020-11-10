dd785cdbfcd7409382014fca8ecb701d2c2448fb
benearnthof <benearnthof@hotmail.de>
2020-11-10 16:49:40 +0100

    add files via upload

---
+++ chess_environment.py
@@ -0,0 +1,15 @@
+# chess environment 
+# functionality for representing and operating on the chess environment
+
+import enum
+import chess 
+import numpy as np
+import copy
+
+# https://github.com/Zeta36/chess-alpha-zero/blob/master/src/chess_zero/env/chess_env.py
+# how to proceed?
+# we can either use the implementation provided here 
+# or write everything in multidimensional arrays in pure functional code
+# i really like functional style but getting better at object oriented
+# programming cant hurt either. 
+# using the prebuilt environment would save a massive amount of time 
